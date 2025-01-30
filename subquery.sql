84645fc7e716d8e32f283b01bf2ceb5710bc5801
Vincent-Hoks <onlytoday752@gmail.com>
2025-01-30 09:50:03 +0700

    stok kosong

--- subquery.sql
+++ subquery.sql
@@ -1,6 +1,4 @@
-
-SELECT barang.* 
-FROM barang
+SELECT barang.* FROM barang
 left JOIN stock_barang ON barang.id_barang = stock_barang.id_barang
 WHERE barang.id_barang
 NOT IN (SELECT id_barang FROM stock_barang)
@@ -12,4 +10,19 @@ left JOIN stock_barang ON barang.id_barang = stock_barang.id_barang
 WHERE barang.id_barang
 NOT IN (SELECT id_barang FROM stock_barang)
 -- meng insert id_barang yang tidak ada di tabel stock_barang ke tabel stock_barang
+
+
+INSERT INTO stock_kosong(id_barang,stock)
+SELECT stock_barang.id_barang,
+		stock_barang.stock
+FROM stock_barang
+WHERE id_stock IN (SELECT stock_barang.id_stock FROM stock_barang WHERE stock = 0)
+-- meng insert id_barang dan stock yang kosong ke tabel stock_kosong
+
+
+
+
+
+
+
 
